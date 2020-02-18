9b9f4132994f9dcadd7a8e96bcbe0a29b09acb9e
xiaen <503962896@qq.com>
2020-02-18 12:46:43 +0800

    完善编辑文章功能

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -2,7 +2,7 @@
   <div class="publish">
     <el-card>
       <div slot="header" class="clearfix">
-        <my-bread>发布文章</my-bread>
+        <my-bread>{{$route.query.id ? '修改' : '发布'}}文章</my-bread>
       </div>
       <el-form label-width="120px">
         <el-form-item label="标题：">
@@ -29,7 +29,10 @@
           <!-- <my-channel :value="articleData.channel_id" @input="articleData.channel_id = $event"></my-channel> -->
           <my-channel v-model="articleData.channel_id"></my-channel>
         </el-form-item>
-        <el-form-item>
+        <el-form-item v-if="$route.query.id">
+          <el-button type="success" @click="update">修改文章</el-button>
+        </el-form-item>
+        <el-form-item v-else>
           <el-button type="primary" @click="submit(false)">发布文章</el-button>
           <el-button @click="submit(true)">存为草稿</el-button>
         </el-form-item>
@@ -71,18 +74,62 @@ export default {
       }
     };
   },
+  created() {
+    this.getImageById();
+  },
   components: { quillEditor },
-  methods: {
-    async submit(draft) {
-      try {
-          await this.$http.post(`articles?draft=${draft}`, this.articleData)
-          this.$message.success(draft ? '存入草稿成功' : '发布文章成功')
-          this.$router.push('/article')
-      } catch(e) {
-        this.$message.error('操作失败')
-      }
+  watch: {
+    "$route.query.id": function() {
+      this.uploadArticleData()
     }
   },
+  methods: {
+    //修改文章
+    async update() {
+      try{
+        await this.$http.put(`articles/${this.$route.query.id}?draft=false`, this.articleData)
+        this.$router.push('/article')
+        this.$message.success('修改成功')
+      } catch(e) {
+        this.$message.error('修改失败')
+      }
+    },
+    // 根据路径id动态更新articleData的数据
+    uploadArticleData() {
+      if (this.$route.query.id) {
+        // 如果有id，获取指定id的数据
+        this.getImageById();
+      } else {
+        //如果没有id，重新赋值
+        this.articleData = {
+          title: null,
+          channel_id: null,
+          content: null,
+          cover: {
+            type: 1,
+            images: []
+          }
+        };
+      }
+    },
+    //获取指定文章
+    async getImageById() {
+      if (this.$route.query.id) {
+        const res = await this.$http.get(`articles/${this.$route.query.id}`);
+        this.articleData = res.data.data;
+      }
+    },
+    //发布文章，存为草稿
+    async submit(draft) {
+      try {
+        await this.$http.post(`articles?draft=${draft}`, this.articleData);
+        this.$message.success(draft ? "存入草稿成功" : "发布文章成功");
+        this.$router.push("/article");
+      } catch (e) {
+        this.$message.error("操作失败");
+      }
+    }
+  }
 };
 </script>
 
